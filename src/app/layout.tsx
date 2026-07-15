7bb76915bfd983dfd0513a88063cb2255bffc8fa
k26 <k26@k26.pl>
2026-07-15 23:41:32 +0200

    Add empty pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,10 @@ import "@fontsource/roboto/700.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Link from "@/components/Link";
 import ButtonLink from "@/components/ButtonLink";
 import theme from "@/theme";
-import {ThemeProvider} from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { StrictMode } from "react";
-
-
 
 export const metadata: Metadata = {
   title: "Ewidencja Faktur",
@@ -30,43 +26,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`h-full antialiased`}>
-      
       <body className="min-h-full flex flex-col">
         <StrictMode>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <AppBar position="static">
-              <Toolbar>
-                <ButtonLink
-                  href="/"
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                Strona główna
-              </ButtonLink>
-                <ButtonLink
-                  href="/rejestr"
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                Rejestr dokumentów
-              </ButtonLink>
-              <ButtonLink
-                  href="/nowy"
-                  
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                Nowy dokument
-              </ButtonLink>
-              <ButtonLink
-                  href="/kategorie"
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                Zarządzanie kategoriami
-              </ButtonLink>
-              </Toolbar>
-            </AppBar>
-            {children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              <AppBar position="static">
+                <Toolbar>
+                  <ButtonLink
+                    href="/"
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    Strona główna
+                  </ButtonLink>
+                  <ButtonLink
+                    href="/rejestr"
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    Rejestr dokumentów
+                  </ButtonLink>
+                  <ButtonLink
+                    href="/nowy"
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    Nowy dokument
+                  </ButtonLink>
+                  <ButtonLink
+                    href="/kategorie"
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    Zarządzanie kategoriami
+                  </ButtonLink>
+                </Toolbar>
+              </AppBar>
+              {children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
         </StrictMode>
       </body>
     </html>
