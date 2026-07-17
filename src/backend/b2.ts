2d4ab948bfabc1d721de8dc6c63d9da74e5f8366
k26 <k26@k26.pl>
2026-07-17 16:22:45 +0200

    add correct cors settings for b2 bucket

--- src/backend/b2.ts
+++ src/backend/b2.ts
@@ -9,6 +9,29 @@ const client = new B2Client({
 });
 
 await client.authorize();
+const bkt = await client.getBucket(process.env.B2_BUCKET!);
+if (!bkt) throw new Error("No bucket configured");
+// If someone can put a hostile page on your localhost
+// you probably have bigger problems.
+// However, in prod settings the localhost origin should be
+// disabled as a good security practice
+// As this is a demo app, i left it here for convience
+const origins = ["http://localhost:3000"];
+if (process.env.ORIGIN) {
+  origins.push(process.env.ORIGIN);
+}
+bkt.update({
+  corsRules: [
+    {
+      allowedOrigins: origins,
+      allowedHeaders: ["content-type"],
+      allowedOperations: ["s3_get", "s3_put"],
+      corsRuleName: "allow-web-access",
+      exposeHeaders: [],
+      maxAgeSeconds: 3600,
+    },
+  ],
+});
 
 export async function getUploadLink() {
   const id = crypto.randomUUID();
